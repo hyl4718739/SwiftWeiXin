09432c8060576b795520c212e21404849e9726fd
hyl4718739 <huangyulin117@163.com>
2015-11-09 17:46:25 +0800

    呵呵，用github试试
    嘎嘎嘎嘎嘎～～

--- SwiftWeiXin/ViewController.swift
+++ SwiftWeiXin/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,RCAnimatedImagesViewDelegate{
 
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var backGroudView: RCAnimatedImagesView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        userText.borderStyle=.UITextBorderStyleNone
+        self.backGroudView.delegate=self;
+        self.backGroudView.startAnimating();
+        print("what's wrong with you!")
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func animatedImagesNumberOfImages(animatedImagesView: RCAnimatedImagesView!) -> UInt {
+        return 2;
+    }
+    
+    func animatedImagesView(animatedImagesView: RCAnimatedImagesView!, imageAtIndex index: UInt) -> UIImage! {
+        return UIImage(named: "image1")
     }
 
     override func didReceiveMemoryWarning() {
